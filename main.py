4fd1a6fa1d3e000d2773d05e4a62d6f49697d978
user <kosyanip@gmail.com>
2020-01-26 12:13:43 +0300

    Implemented data splitting

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-# 2502 + 1402 = 3904 easy ham
-# 502  + 1398 = 1900 spam (* 2 = 3800)
-#             =  252 hard ham
-
 # spam is a positive class
-
-# due to data class imbalance spam examples will be duplicated
-# split data 80-20 train-test:
-# train:
-# 3120 easy ham
-# 200  hard ham (combines with easy ham for now)
-# 3040 spam
 
 # features        | interpretation|
 # ---------------------------------
@@ -24,6 +13,7 @@
 
 
 import matplotlib.pyplot as plt
+from split_original import split_files
 
 
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
@@ -42,6 +32,9 @@ def plot_roc_curve(fpr, tpr, label=None):
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
     plt.show()
+
+
+split_files()
 
 
 
---
+++ split_original.py
@@ -0,0 +1,58 @@
+# 2502 + 1402 = 3904 easy ham
+# 502  + 1398 = 1900 spam (* 2 = 3800)
+#             =  252 hard ham
+
+# due to data class imbalance spam examples will be duplicated
+# split data 80-20 train-test:
+# train:
+# 3120 easy ham
+# 200  hard ham (may be merged easy ham)
+# 3040 spam
+
+
+from os.path import isfile, isdir, join
+from os import listdir, makedirs
+from shutil import copyfile
+
+
+def split_files(train_ratio=0.8):
+
+    # getting files
+    path = "original data"
+    ham_dirs = ["easy_ham", "easy_ham_2", "hard_ham"]
+    spam_dirs = ["spam", "spam_2"]
+
+    def get_files(dirs):
+        res = []
+        for dir in dirs:
+            full_path = join(path, dir)
+            res.extend([join(full_path, f) for f in listdir(full_path) if isfile(join(full_path, f))])
+        return res
+
+    ham_files = get_files(ham_dirs)
+    spam_files = get_files(spam_dirs)
+
+    if not isdir("train"):
+        makedirs("train")
+    if not isdir("test"):
+        makedirs("test")
+
+    # splitting
+    def copy_ham(files, to):
+        for file in files:
+            # getting part of filename like this may be troublesome on windows
+            # because of the '/' symbol
+            copyfile(file, join(to, "ham.{}".format(file.split('/')[-1].split('.')[-1])))
+
+    def copy_spam(files, to, duplicate=True):
+        for file in files:
+            copyfile(file, join(to, "spam.{}".format(file.split('/')[-1].split('.')[-1])))
+            if duplicate:
+                copyfile(file, join(to, "spam.{}COPY".format(file.split('/')[-1].split('.')[-1])))
+
+    # train
+    copy_ham(ham_files[:int(len(ham_files) * train_ratio)], "train")
+    copy_spam(spam_files[:int(len(spam_files) * train_ratio)], "train")
+    # test
+    copy_ham(ham_files[int(len(ham_files) * train_ratio):], "test")
+    copy_spam(spam_files[int(len(spam_files) * train_ratio):], "test")

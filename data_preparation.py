0537917a8450067783551332d254b97aaecb7972
user <kosyanip@gmail.com>
2020-02-03 18:22:31 +0300

    Reworked data extraction. Unknown words count could be negative (fixed) Now using default library that actually helps to extract text from mails properly. Now instead of manually deselecting meaningless words (like "this", "that", "the", that are met in almost every message) top 20 most frequent words are actually dropped. Implemented a vocabulary construction parameter that allows to manually pick words to search in messages.

--- data_preparation.py
+++ data_preparation.py
@@ -3,8 +3,13 @@ import re
 from os.path import isfile, join
 from os import listdir
 
-from sklearn.base import BaseEstimator, TransformerMixin
-from pandas import DataFrame
+# from sklearn.base import BaseEstimator, TransformerMixin
+from pandas import DataFrame, Series
+from matplotlib import pyplot as plt
+from email import message_from_file
+from email.policy import default as default_policy
+from email import message
+from split_original import get_files
 
 
 # features                          | interpretation  |
@@ -19,22 +24,23 @@ from pandas import DataFrame
 # ----------------------------------|----------------|
 
 
-# Extracts features on the top 
-class FeatureExtractor(BaseEstimator, TransformerMixin):
+# Extracts features on the top
+# class FeatureExtractor(BaseEstimator, TransformerMixin):
+class FeatureExtractor:
     # Regular expressions used while fitting and transforming
     _html_tag_re = re.compile("<[^>]*>", re.IGNORECASE)
     _prefix_re = re.compile("\n(?!http|dns|ftp|url)([a-z0-9-_]+):", re.IGNORECASE)
-    _valuable_word_re = re.compile(
-        "(?!(?:[-=_+.@]+|are|the|and|www|net|org|com|for|any|int|edu|gov|mil|html)[^a-z])[a-z-_]{3,}", re.IGNORECASE)
+    _valuable_word_re = re.compile("(?![^a-z])[a-z-_]{3,}", re.IGNORECASE)
     _url_re = re.compile("http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-f][0-9a-f]))+", re.IGNORECASE)
     _word_re = re.compile("(?![-=_+.@]+)[a-z-_]{2,}", re.IGNORECASE)
     _cap_word_re = re.compile("(?![-=_+.@]+)[A-Z-_]{2,}")
     _domain_re = re.compile("<?[^<]+\.[a-zA-Z]{2,3}>?", re.IGNORECASE)
+    _hash_like_re = re.compile("[^\s]{20,}")
+    _replied_re = re.compile(">[^\n]*")
 
     def __init__(self, max_dictionary_size=1000, count_total_tags=True, count_total_words=True, count_total_links=True,
                  count_total_caps_words=True, count_unknown_words=True, extract_sender_domain=True,
-                 extract_receiver_domain=True,
-                 ):
+                 extract_receiver_domain=True, dictionary_strategy="auto", words_to_search=None):
         """
         :param max_dictionary_size: Maximum number of words that can be remembered -1 if no limit
         :param count_total_tags: Whether or not should it evaluate html tags as words
@@ -44,6 +50,10 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         :param count_unknown_words:  Whether or not should it count all unknown words in each mail
         :param extract_sender_domain:  Whether or not should it extract sender domain
         :param extract_receiver_domain: Whether or not should it extract receiver domain
+        :param dictionary_strategy: Strategy of learning words.
+        "auto"   - most frequent (starting from top 20) will be searched
+        "manual" - selected words will be searched. In this case "words_to_search" should be specified
+        :param words_to_search: List of words that should be learnt
         """
         self.max_dict_size = max_dictionary_size
         self.extracting_features = {
@@ -56,22 +66,56 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             "extract receiver domain": extract_receiver_domain
         }
 
+        if dictionary_strategy == "manual":
+            if words_to_search is None:
+                raise AttributeError("dictionary_strategy is manual, but words_to_search is not specified")
+            self.max_dict_size = len(words_to_search)
+
+        self.strategy = dictionary_strategy
+        self.words_to_search = words_to_search
+
         # Contains "word: count" pairs
         self.vocabulary = dict()
-        # Mail prefixes after which actual message may begin
-        # i.e. parameters like route, receiver, sender etc.
-        self._mail_prefixes = set()
 
-    def _get_mail_text(self, mail):
-        mail_text = mail[max([mail.find(prefix) for prefix in self._mail_prefixes]) + 1:]
-        return mail_text[mail_text.find('\n') + 1:].strip("\n ")
+    def _filter_mail_text(self, mail_text):
+        return re.sub(self._html_tag_re, "",
+                      re.sub(self._url_re, "",
+                             re.sub(self._replied_re, "",
+                                    re.sub(self._hash_like_re, "", mail_text))))
 
-    def _extract_by_prefix(self, mail, prefix):
-        prefix_index = mail.find(prefix)
-        if prefix_index == -1:
-            return None
+    def _decode_part(self, part, erase_html_tags=True):
+        content_type = part.get_content_type()
+        charset = part.get_content_charset()
+        if charset is None:
+            charset = "utf-8"
         else:
-            return mail[prefix_index + len(prefix) + 1: prefix_index + mail[prefix_index + 1:].find('\n') + 1]
+            charset = re.sub(re.compile("_?charset_?", re.IGNORECASE), "", charset)
+        if charset == "default" or charset == "unknown-8bit":
+            charset = "utf-8"
+        if charset == "chinesebig5":
+            charset = "big5"
+        if content_type.lower().startswith("text"):
+            part_text = part.get_payload(decode=True).decode(charset, errors="ignore")
+            if content_type == "text/html":
+                if erase_html_tags:
+                    return re.sub(self._html_tag_re, "", part_text)
+                return part_text
+            if content_type.startswith("text") and not content_type.endswith("headers"):
+                return part_text
+
+    def _extract_message(self, mail, erase_html_tags=True):
+        mail_text = str(mail["Subject"])
+        for part in mail.walk():
+            mail_text = "{}\n{}".format(mail_text, self._decode_part(part), erase_html_tags)
+        return mail_text
+
+    def _get_mail_text(self, mail_path):
+        # There are non-ASCII characters which are causing errors
+        mail = message_from_file(open(mail_path, encoding="utf-8", errors="ignore"), policy=default_policy)
+
+        mail_text = self._extract_message(mail)
+
+        return self._filter_mail_text(mail_text)
 
     # Construct a vocabulary
     def fit(self, X, y=None, verbose=False):
@@ -81,78 +125,48 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         :param verbose: Whether or not it should print progress
         :return: self
         """
+        if self.strategy == "manual":
+            print("Manual strategy is chosen. Not looking through mails")
+            self.vocabulary = set(self.words_to_search)
+            return self
         if verbose:
             print("Started fitting mails...")
 
-        # Reading all the mails and keeping them in memory
-        # There are non-ASCII characters which are causing errors
-        mails = [open(file_path, 'r', encoding="utf-8", errors="ignore").read() for file_path in X]
-
-        if verbose:
-            print("Finding prefixes...")
-        # First run
-        # Finding prefixes
-        mails_matches = [set(re.findall(self._prefix_re, mail)) for mail in mails]
-        unfiltered_prefixes = dict()
-        for mail_matches in mails_matches:
-            for match in mail_matches:
-                if match in unfiltered_prefixes:
-                    unfiltered_prefixes[match] += 1
-                else:
-                    unfiltered_prefixes[match] = 1
-
-        for prefix, count in unfiltered_prefixes.items():
-            if count > 90 or prefix.startswith("X-") or prefix.startswith("List-"):
-                self._mail_prefixes.add("\n{}:".format(prefix))
-        del unfiltered_prefixes
-        if verbose:
-            print("Prefixes found")
-
-        # Second run
         # Learning most frequent words in lower case
         unfiltered_vocabulary = dict()
 
         if verbose:
-            print("Learning up to {} most frequent words...".format(self.max_dict_size))
+            if self.max_dict_size > 0:
+                print("Selecting up to {} most frequent words...".format(self.max_dict_size))
+            elif self.max_dict_size == -1:
+                print("Selecting all met words")
 
-        for mail in mails:
-            mail_text = self._get_mail_text(mail)
+        for mail_path in X:
 
-            subject_index = mail.find("\nSubject:")
-            if subject_index != -1:
-                mail_text += '\n' + mail[subject_index + 9: mail[subject_index + 9:].find('\n')].strip()
-
-            # Cleaning mail out of html tags
-            mail_text = re.sub(self._html_tag_re, "", mail_text).strip("\n ")
-            # and out of links
-            mail_text = re.sub(self._url_re, "", mail_text)
-
-            words = set(re.findall(self._valuable_word_re, mail_text))
+            text = self._get_mail_text(mail_path)
+            words = set(re.findall(self._valuable_word_re, text))
             for word in words:
                 word = word.lower()
                 if word in unfiltered_vocabulary:
                     unfiltered_vocabulary[word] += 1
                 else:
                     unfiltered_vocabulary[word] = 1
-        self.vocabulary = {word: unfiltered_vocabulary[word] for word in sorted(unfiltered_vocabulary,
-                                                                                key=unfiltered_vocabulary.__getitem__,
-                                                                                reverse=True)[:self.max_dict_size]}
+
+        self.vocabulary = {word for word in sorted(unfiltered_vocabulary,
+                                                   key=unfiltered_vocabulary.__getitem__,
+                                                   reverse=True)[20:self.max_dict_size]}
         if verbose:
             print("Words learned\nFitting finished\n")
         return self
 
     def _extract_domain(self, mail, prefix):
-        value = self._extract_by_prefix(mail, prefix)
-        if value is None:
-            return "unknown"
-        else:
-            regexp_result = re.findall(self._domain_re, value)
+        if prefix in mail.keys():
+            regexp_result = re.findall(self._domain_re, mail[prefix])
             if len(regexp_result) > 0:
                 buff = regexp_result[0].strip("<>")
                 buff = buff[len(buff) - 3: len(buff)]
                 return buff.lstrip('.').lower()
-            else:
-                return "unknown"
+        return "unknown"
 
     # Apply regular expressions
     def transform(self, X, y=None, verbose=False):
@@ -165,12 +179,10 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         if verbose:
             print("Started transforming mails...")
 
-        mails = [open(file_path, 'r', encoding="utf-8", errors="ignore").read() for file_path in X]
-        # Third run
         # Extracting features
         df_columns = [feature_name[feature_name.find(' ') + 1:]
                       for feature_name, collect in self.extracting_features.items() if collect]
-        df_columns.extend([word for word in self.vocabulary])
+        df_columns.extend(list(self.vocabulary))
         df_columns.append("spam")
 
         result = DataFrame(columns=df_columns)
@@ -181,45 +193,48 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         if verbose:
             print("Extracting data...")
 
-        for full_path, mail in zip(X, mails):
+        for mail_path in X:
             if verbose:
-                if current_mail_number > int(len(X) * current_ten_percentile / 100):
+                if current_mail_number > len(X) * current_ten_percentile // 100:
                     print("Finished {}%".format(current_ten_percentile))
                     current_ten_percentile += 10
             feature_line = []
-            mail_text = self._get_mail_text(mail)
-            message = re.sub(self._url_re, "", re.sub(self._html_tag_re, "", mail_text)).strip("\n ")
+            mail = message_from_file(open(mail_path, encoding="utf-8", errors="ignore"), policy=default_policy)
+
+            mail_text = self._extract_message(mail, erase_html_tags=False)
+
+            filtered_mail = self._filter_mail_text(mail_text)
 
             # Adding features
             if self.extracting_features["count total tags"]:
                 feature_line.append(len(re.findall(self._html_tag_re, mail_text)))
 
-            total_words_count = len(re.findall(self._word_re, message))
+            total_words_count = len(re.findall(self._word_re, filtered_mail))
             if self.extracting_features["count total words"]:
                 feature_line.append(total_words_count)
 
             if self.extracting_features["count total caps words"]:
-                feature_line.append(len(re.findall(self._cap_word_re, message)))
+                feature_line.append(len(re.findall(self._cap_word_re, filtered_mail)))
 
             if self.extracting_features["count total links"]:
                 feature_line.append(len(re.findall(self._url_re, mail_text)))
 
-            known_words_counts = [len(re.findall(re.compile(word, re.IGNORECASE), message))
-                                  for word in self.vocabulary.keys()]
+            known_words_counts = [len(re.findall(re.compile(word, re.IGNORECASE), filtered_mail))
+                                  for word in self.vocabulary]
             if self.extracting_features["count unknown words"]:
                 feature_line.append(total_words_count - len(known_words_counts))
 
             if self.extracting_features["extract sender domain"]:
-                feature_line.append(self._extract_domain(mail, "\nFrom:"))
+                feature_line.append(self._extract_domain(mail, "From"))
 
             if self.extracting_features["extract receiver domain"]:
-                feature_line.append(self._extract_domain(mail, "\nTo:"))
+                feature_line.append(self._extract_domain(mail, "To"))
 
             # Adding known words
             feature_line.extend(known_words_counts)
 
             # Adding labels
-            feature_line.append(full_path.split('/')[-1].split('.')[0] == "spam")
+            feature_line.append(mail_path.split('/')[-1].split('.')[0] == "spam")
 
             # Adding to dataframe
             result.loc[result.shape[0]] = feature_line
@@ -233,3 +248,10 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
 
     def fit_transform(self, X, y=None, verbose=False, **fit_params):
         return self.fit(X, verbose=verbose).transform(X, verbose=verbose)
+
+
+if __name__ == "__main__":
+    fe = FeatureExtractor(max_dictionary_size=20)
+    result = fe.fit_transform(get_files(["train"]), verbose=True)
+    print(result)
+    print("Here")
